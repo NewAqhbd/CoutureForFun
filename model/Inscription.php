11050e75d9530205caa95b71053cd74ba8d3130a
NewAqhbd <brice.murat@protonmail.com>
2024-04-04 15:15:01 +0200

    Chiffrement du mot de passe

--- model/Inscription.php
+++ model/Inscription.php
@@ -3,6 +3,7 @@ require "../model/Connexion.php";
 
 function create_account($mail, $mdp) {
     global $con;
+    $encrypted_mdp = md5($mdp);
     $sql_check_mail = "SELECT email FROM utilisateur WHERE email = :mail";
     $req_check_mail = $con->prepare($sql_check_mail);
     $req_check_mail->bindValue(":mail", $mail, PDO::PARAM_STR);
@@ -12,7 +13,7 @@ function create_account($mail, $mdp) {
         $sql_create_user = "INSERT INTO utilisateur(email, mdp) VALUES(:email, :mdp)";
         $req_create_user = $con->prepare($sql_create_user);
         $req_create_user->bindValue(":email", $mail, PDO::PARAM_STR);
-        $req_create_user->bindValue(":mdp", $mdp, PDO::PARAM_STR);
+        $req_create_user->bindValue(":mdp", $encrypted_mdp, PDO::PARAM_STR);
 
         try {
             $req_create_user->execute();
